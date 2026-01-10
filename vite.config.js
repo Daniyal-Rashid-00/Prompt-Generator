1fc6c65be87e17bacf8de85a7ce936c2ec2900e6
Daniyal-Rashid-00 <everything4u1212@gmail.com>
2026-01-10 18:50:35 +0500

    Fix v2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/Prompt-Generator/',
+    base: './',
     build: {
         outDir: 'dist',
         sourcemap: false,
