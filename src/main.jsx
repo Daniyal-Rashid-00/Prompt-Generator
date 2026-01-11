0a0a01752e37f72a42302765c3befec07f0930e7
Daniyal-Rashid-00 <everything4u1212@gmail.com>
2026-01-11 11:23:33 +0500

    Fix v7

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <div style={{ color: 'white', padding: '20px', fontSize: '24px' }}>
+            <h1>React is Running!</h1>
+            <p>If you see this, the build and deployment are working.</p>
+        </div>
     </React.StrictMode>,
 )
